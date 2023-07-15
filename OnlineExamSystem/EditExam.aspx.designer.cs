40968693b0b69b9c29887a4e53582aa66918af90
cleverman <first1124.dev.master@gmail.com>
2023-07-15 01:07:43 -0700

    third

--- OnlineExamSystem/EditExam.aspx.designer.cs
+++ OnlineExamSystem/EditExam.aspx.designer.cs
@@ -112,14 +112,5 @@ namespace OnlineExamSystem
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button eMcqB;
-
-        /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
